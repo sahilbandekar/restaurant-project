3dc0a08087871848b8c49ece3fabbde70363ad29
Sahil Arvind Bandekar <sahilbandekar21@gmail.com>
2023-03-22 17:59:10 +0530

    Creating Signup Component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <SignUp />
 </template>
 
 <script>
+import SignUp from './components/SignUp.vue';
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    SignUp
+  }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+/* SignUp.vue & LogIn.vue CCS STARTED */
+.wrapper {
+  width: 30%;
+  text-align: center;
+  box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
+  padding: 10px;
+  margin: 100px auto;
+  border-radius: 12px;
+}
+
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid skyblue;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  color: white;
+  background-color: skyblue;
+  cursor: pointer;
+}
+
+.formFooter {
+  display: flex;
+  justify-content: center;
+}
+
+.formFooter p:first-child {
+  padding-right: 10px;
 }
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="register">
+        <div class="wrapper">
+            <img style="width: 100px;" src="../assets/resto-logo.jpg">
+            <h2>SignUp</h2>
+            <input type="text" placeholder="Enter Name">
+            <input type="email" placeholder="Enter Email">
+            <input type="password" placeholder="Enter Password">
+            <button>Sign Up</button>
+            <div class="formFooter">
+                <p>Already have an account?</p>
+                <p>Login</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp',
+}
+</script>
+
+<style></style>
