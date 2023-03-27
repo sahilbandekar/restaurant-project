84f967dd0279feb4468feb659e950a322b2f5acb
Sahil Arvind Bandekar <sahilbandekar21@gmail.com>
2023-03-28 00:19:32 +0530

    Creating API_Integrating and displaying restaurant

--- src/components/HomeCmp.vue
+++ src/components/HomeCmp.vue
@@ -1,24 +1,40 @@
 <template>
     <HeaDer />
     <h1>Hello {{this.name}}, Welcome to Home Page</h1>
+    <table border="1">
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Contact</td>
+            <td>Address</td>
+        </tr>
+        <tr v-for="item in restaurants" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.contact }}</td>
+            <td>{{ item.address }}</td>
+        </tr>
+    </table>
 </template>
 
 
 <script>
 
 import HeaDer from './HeaDer.vue';
+import axios from 'axios';
 
 export default {
     name: 'HomeCmp',
     data(){
         return {
-            name: ''
+            name: '',
+            restaurants: []
         }
     },
     components: {
         HeaDer
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem("user-info");
         // to show name 
         this.name = JSON.parse(user).name
@@ -26,8 +42,23 @@ export default {
         if (!user) {
             this.$router.push({ name: 'SignUp' })
         }
+
+
+        let result = await axios.get('http://localhost:3000/restaurants');
+        console.log(result)
+        this.restaurants = result.data;
+        
     }
 }
 </script>
 
-<style></style>
+<style>
+
+td{
+    width: 120px;
+    height: 40px;
+}
+table{
+    margin: 0 auto;
+}
+</style>
